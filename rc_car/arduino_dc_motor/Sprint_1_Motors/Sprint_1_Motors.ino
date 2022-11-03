883dc70b8db39b82ef544436a8aa133283c7f246
ayushchakra <achakraborty@olin.edu>
2022-11-02 23:40:36 -0400

    added keyboard input to loop

--- rc_car/arduino_dc_motor/Sprint_1_Motors/Sprint_1_Motors.ino
+++ rc_car/arduino_dc_motor/Sprint_1_Motors/Sprint_1_Motors.ino
@@ -6,6 +6,8 @@ Adafruit_DCMotor *frontRightMotor = AFMS.getMotor(1);       //assign the left mo
 Adafruit_DCMotor *backLeftMotor = AFMS.getMotor(3);      //assign the right motor to port M1
 Adafruit_DCMotor *backRightMotor = AFMS.getMotor(4);       //assign the left motor to port M3
 
+int currDriveMode = 0;
+
 void setup() {
   Serial.flush();                                     //reset serial monitor
   Serial.begin(9600);
@@ -14,7 +16,33 @@ void setup() {
 }
 
 void loop() {
-  driveDiagNE();
+  Serial.println(currDriveMode);
+  if (Serial.available() > 0) {
+    currDriveMode = Serial.parseInt();
+  }
+
+  switch (currDriveMode) {
+    case 1:
+      driveNorth();
+    case 2:
+      driveSouth();
+    case 3:
+      driveEast();
+    case 4:
+      driveWest();
+    case 5:
+      driveDiagNE();
+    case 6:
+      driveDiagNW();
+    case 7:
+      driveDiagSE();
+    case 8:
+      driveDiagSW();
+    case 9:
+      rotateClock();
+    case 10:
+      rotateCounterClock();
+  }
 }
 
 void driveNorth(){         //drive straight
