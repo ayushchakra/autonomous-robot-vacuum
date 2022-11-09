625267c2ae4577a23ef22878ac93ce747e7baef8
Ubuntu <ubuntu@ubuntu.olin.edu>
2022-11-09 01:29:30 +0000

    removed plotitng

--- lidar_playground/test.py
+++ lidar_playground/test.py
@@ -11,15 +11,15 @@ import numpy as np
 x = []
 y = []
 
-with Sweep('/dev/ttyUSB0') as sweep:
+with Sweep('/dev/ttyUSB1') as sweep:
     sweep.start_scanning()
 
-    plt.ion()
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    line1, = ax.plot(x,y,'b.')
-    ax.set_xbound(lower=-250, upper=250)
-    ax.set_ybound(lower=-250, upper=250)
+#    plt.ion()
+#    fig = plt.figure()
+#    ax = fig.add_subplot(111)
+#    line1, = ax.plot(x,y,'b.')
+#    ax.set_xbound(lower=-250, upper=250)
+#    ax.set_ybound(lower=-250, upper=250)
 
     for scan in sweep.get_scans():
         angles = []
@@ -29,11 +29,12 @@ with Sweep('/dev/ttyUSB0') as sweep:
             distances.append(sample.distance)
         angles = np.array(angles)
         distances = np.array(distances)
+        pdb.set_trace()
         x = distances*(np.cos(np.deg2rad(angles/1000)))
         y = distances*(np.sin(np.deg2rad(angles/1000)))
 
-        line1.set_xdata(x)
-        line1.set_ydata(y)
-        fig.canvas.draw()
-        fig.canvas.flush_events()
+ #       line1.set_xdata(x)
+ #       line1.set_ydata(y)
+ #       fig.canvas.draw()
+ #       fig.canvas.flush_events()
             
