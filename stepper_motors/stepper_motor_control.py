5558e747f00bb5175030e2d9374f54d04eb580d2
Jiangster <ljiang@olin.edu>
2022-11-11 23:59:14 -0500

    motors 1 and 3 work

--- stepper_motors/stepper_motor_control.py
+++ stepper_motors/stepper_motor_control.py
@@ -1,105 +1,97 @@
-'''
-Basic script to run a bipolar stepper motor with 200 steps/rev.
-
-Relies on RpiMotorLib. Install found here:
-https://github.com/gavinlyonsrepo/RpiMotorLib
-
-Nice wiring tutorial here:
-https://github.com/gavinlyonsrepo/RpiMotorLib/blob/master/Documentation/Nema11L298N.md
-
-L298 stepper tester file:
-https://github.com/gavinlyonsrepo/RpiMotorLib/blob/master/test/L298_Step_Test.py
-'''
-
-import time
 import RPi.GPIO as GPIO
+import time
 from RpiMotorLib import RpiMotorLib
 
 
-# Black, Green, Red, Blue
-GpioPinsOne = [7, 11, 13, 15]
-GpioPinsTwo = [12, 16, 18, 22]
-GpioPinsThree = [29, 31, 33, 35]
-GpioPinsFour = [32, 36, 38, 40]
+mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "Nema")
+
+# Connect GPIO to [IN1 , IN2 , IN3 ,IN4] on Motor PCB
+GpioPins = [7, 11, 13, 15]
+
+# 
+MOTOR_ONE_PINS = [4, 17, 27, 22]  # [7, 0, 2, 3]
+MOTOR_TWO_PINS = [6, 13, 19, 26]
+MOTOR_THREE_PINS = [21,20,16,12]  # [12,16,20,21]
+MOTOR_FOUR_PINS = [35, 37, 38, 40]
 
 
-front_left_motor = RpiMotorLib.BYJMotor("MotorOne", "Nema")
-front_right_motor = RpiMotorLib.BYJMotor("MotorTwo", "Nema")
-back_left_motor = RpiMotorLib.BYJMotor("MotorThree", "Nema")
-back_right_motor = RpiMotorLib.BYJMotor("MotorFour", "Nema")
 
 
-def one_phase(self):
-	'''
-	Activates the motor using one phase.
-	'''
-	pass
+# step 1 - high low high low
+def phase_one(motor_array):
+    GPIO.output(motor_array[0], GPIO.HIGH)
+    GPIO.output(motor_array[1], GPIO.LOW)
+    GPIO.output(motor_array[2], GPIO.HIGH)
+    GPIO.output(motor_array[3], GPIO.LOW)
+
+# step 2 - low high high low
+def phase_two(motor_array):
+    GPIO.output(motor_array[0], GPIO.LOW)
+    GPIO.output(motor_array[1], GPIO.HIGH)
+    GPIO.output(motor_array[2], GPIO.HIGH)
+    GPIO.output(motor_array[3], GPIO.LOW)
+
+# step 3 - low high low high
+def phase_three(motor_array):
+    GPIO.output(motor_array[0], GPIO.LOW)
+    GPIO.output(motor_array[1], GPIO.HIGH)
+    GPIO.output(motor_array[2], GPIO.LOW)
+    GPIO.output(motor_array[3], GPIO.HIGH)
+
+# step 4 - high low low high
+def phase_four(motor_array):
+    GPIO.output(motor_array[0], GPIO.HIGH)
+    GPIO.output(motor_array[1], GPIO.LOW)
+    GPIO.output(motor_array[2], GPIO.LOW)
+    GPIO.output(motor_array[3], GPIO.HIGH)
 
 
-# More torque
-def two_phase(self):
-	'''
-	Activates the motor using both phases.
-	'''
-	pass
-
-
-# More precise, twice as many steps
-def half_step(self, motor, step_delay, steps, counterclockwise):
-	# Arguments for motor run function
+def main():
+    """
+    main function loop.
+    """
+    for pin in MOTOR_ONE_PINS:
+        GPIO.setup(pin, GPIO.OUT)
+    
+    for pin in MOTOR_THREE_PINS:
+        GPIO.setup(pin, GPIO.OUT)
+    
+    for i in range(100):
+        phase_one(MOTOR_ONE_PINS)
+        phase_one(MOTOR_THREE_PINS)
+        time.sleep(0.005)
+        
+        phase_two(MOTOR_ONE_PINS)
+        phase_two(MOTOR_THREE_PINS)
+        time.sleep(0.005)
+        
+        phase_three(MOTOR_ONE_PINS)
+        phase_three(MOTOR_THREE_PINS)
+        time.sleep(0.005)
+        
+        phase_four(MOTOR_ONE_PINS)
+        phase_four(MOTOR_THREE_PINS)
+        time.sleep(0.005)
+    
+    
+    
+    # step 2
+    
+    # Arguments  for motor run function
     # (GPIOPins, stepdelay, steps, counterclockwise, verbose, steptype, initdelay)
-    motor.motor_run(GpioPinsOne, step_delay, steps, counterclockwise, True, "half", 1)
-	pass
+#     input("Press <Enter> to test motor 1")
+#     mymotortest.motor_run(MOTOR_ONE_PINS,.5, 50, False, True,"full" ,1 )
+#     time.sleep(1)
+#     
+#     input("Press <Enter> to test motor 2")
+#     mymotortest.motor_run(MOTOR_TWO_PINS,.2, 10, False, True,"full", 1)
+#     time.sleep(1) 
+#     input("Press <Enter> to test motor 3")
+#     mymotortest.motor_run(MOTOR_THREE_PINS,.05, 25, True, True,"half", 1)
+#     time.sleep(1)
+#     input("Press <Enter> to test motor 4")
+#     mymotortest.motor_run(MOTOR_FOUR_PINS,.02,25,False,False,"half", 3)
 
 
-def move_north(self, step_delay, steps):
-	'''
-	Moves all four wheels forward.
-	'''
-	half_step(front_left_motor, step_delay, steps, True)
-	half_step(front_right_motor, step_delay, steps, False)
-	half_step(back_left_motor, step_delay, steps, True)
-	half_step(back_right_motor, step_delay, steps, False)
-
-
-def move_west(self):
-	'''
-	front left: backward
-	front right: forward
-	back left: forward
-	back right: backward
-	'''
-	half_step(front_left_motor, step_delay, steps, False)
-	half_step(front_right_motor, step_delay, steps, False)
-	half_step(back_left_motor, step_delay, steps, True)
-	half_step(back_right_motor, step_delay, steps, True)
-
-
-def move_east(self, step_delay, steps):
-	'''
-	front left: forward
-	front right: backward
-	back left: backward
-	back right: forward
-	'''
-	half_step(front_left_motor, step_delay, steps, True)
-	half_step(front_right_motor, step_delay, steps, True)
-	half_step(back_left_motor, step_delay, steps, False)
-	half_step(back_right_motor, step_delay, steps, False)
-
-
-def move_south(self, step_delay, steps):
-	'''
-	Moves all four wheels backward.
-	'''
-	half_step(front_left_motor, step_delay, steps, False)
-	half_step(front_right_motor, step_delay, steps, True)
-	half_step(back_left_motor, step_delay, steps, False)
-	half_step(back_right_motor, step_delay, steps, True)
-
-
-
-
-move_north(0.05, 25)
-GPIO.cleanup() # Optional
-exit()
+main()
+GPIO.cleanup()
