4f285f4c6a25dd4233775e5f59c31691730d6533
Lily Jiang <ljiang@olin.edu>
2022-12-05 17:55:05 -0500

    update motor 4 for A4988 driver

--- sprint_two_rc/sprint_two_rc/stepper_motors/stepper_motor_control.py
+++ sprint_two_rc/sprint_two_rc/stepper_motors/stepper_motor_control.py
@@ -7,7 +7,11 @@ import time
 MOTOR_ONE_PINS = [7, 11, 13, 15]    # back left
 MOTOR_TWO_PINS = [12, 16, 18, 22]   # back right
 MOTOR_THREE_PINS = [29, 31, 33, 35]  # front left
-MOTOR_FOUR_PINS = [32, 36, 38, 40]  # front right
+
+MOTOR_FOUR_PINS = [38, 40]  # this is the step + direction pins
+
+# Front right is using a different driver now, so ignore
+# MOTOR_FOUR_PINS = [32, 36, 38, 40]  # front right
 
 
 
@@ -20,7 +24,6 @@ TIME_SLEEP = 0.004
 
 def setup_pins(motor_pins):
     for pin in motor_pins:
-        print("pin", pin)
         GPIO.setup(pin, GPIO.OUT)
 
 def setup_all_pins():
@@ -29,7 +32,11 @@ def setup_all_pins():
     setup_pins(MOTOR_ONE_PINS)
     setup_pins(MOTOR_TWO_PINS)
     setup_pins(MOTOR_THREE_PINS)
-    setup_pins(MOTOR_FOUR_PINS)
+    # setup_pins(MOTOR_FOUR_PINS)
+
+    # setup front right pins separately
+    GPIO.setup(MOTOR_FOUR_PINS[0], GPIO.OUT)
+    GPIO.setup(MOTOR_FOUR_PINS[1], GPIO.OUT)
     print("done setting up pins")
 
 # step 1 - high low high low
@@ -89,25 +96,29 @@ def run_motors(motor_one_pins=MOTOR_ONE_PINS, motor_two_pins=MOTOR_TWO_PINS, mot
         phase_one(motor_one_pins)
         phase_one(motor_two_pins)
         phase_one(motor_three_pins)
-        phase_one(motor_four_pins)
+        # phase_one(motor_four_pins)
+        GPIO.output(MOTOR_FOUR_PINS[0], GPIO.HIGH)
         time.sleep(time_sleep)
         
         phase_two(motor_one_pins)
         phase_two(motor_two_pins)
         phase_two(motor_three_pins)
-        phase_two(motor_four_pins)
+        # phase_two(motor_four_pins)
+        GPIO.output(MOTOR_FOUR_PINS[0], GPIO.LOW)
         time.sleep(time_sleep)
         
         phase_three(motor_one_pins)
         phase_three(motor_two_pins)
         phase_three(motor_three_pins)
-        phase_three(motor_four_pins)
+        # phase_three(motor_four_pins)
+        GPIO.output(MOTOR_FOUR_PINS[0], GPIO.HIGH)
         time.sleep(time_sleep)
         
         phase_four(motor_one_pins)
         phase_four(motor_two_pins)
         phase_four(motor_three_pins)
-        phase_four(motor_four_pins)
+        # phase_four(motor_four_pins)
+        GPIO.output(MOTOR_FOUR_PINS[0], GPIO.LOW)
         time.sleep(time_sleep)
 
 
@@ -119,25 +130,25 @@ def reverse_motors(motor_one_pins=MOTOR_ONE_PINS, motor_two_pins=MOTOR_TWO_PINS,
         phase_one_reversed(motor_one_pins)
         phase_one_reversed(motor_two_pins)
         phase_one_reversed(motor_three_pins)
-        phase_one_reversed(motor_four_pins)
+        # phase_one_reversed(motor_four_pins)
         time.sleep(time_sleep)
         
         phase_two(motor_one_pins)
         phase_two(motor_two_pins)
         phase_two(motor_three_pins)
-        phase_two(motor_four_pins)
+        # phase_two(motor_four_pins)
         time.sleep(time_sleep)
         
         phase_three_reversed(motor_one_pins)
         phase_three_reversed(motor_two_pins)
         phase_three_reversed(motor_three_pins)
-        phase_three_reversed(motor_four_pins)
+        # phase_three_reversed(motor_four_pins)
         time.sleep(time_sleep)
         
         phase_four(motor_one_pins)
         phase_four(motor_two_pins)
         phase_four(motor_three_pins)
-        phase_four(motor_four_pins)
+        # phase_four(motor_four_pins)
         time.sleep(time_sleep)
 
 
@@ -153,25 +164,29 @@ def drive_north(motor_one_pins=MOTOR_ONE_PINS, motor_two_pins=MOTOR_TWO_PINS, mo
         phase_one_reversed(MOTOR_ONE_PINS)
         phase_one(MOTOR_TWO_PINS)
         phase_one_reversed(MOTOR_THREE_PINS)
-        phase_one(MOTOR_FOUR_PINS)
+        # phase_one(MOTOR_FOUR_PINS)
+        GPIO.output(MOTOR_FOUR_PINS[0], GPIO.HIGH)
         time.sleep(time_sleep)
     
         phase_two(MOTOR_ONE_PINS)
         phase_two(MOTOR_TWO_PINS)
         phase_two(MOTOR_THREE_PINS)
-        phase_two(MOTOR_FOUR_PINS)
+        # phase_two(MOTOR_FOUR_PINS)
+        GPIO.output(MOTOR_FOUR_PINS[0], GPIO.LOW)
         time.sleep(time_sleep)
     
         phase_three_reversed(MOTOR_ONE_PINS)
         phase_three(MOTOR_TWO_PINS)
         phase_three_reversed(MOTOR_THREE_PINS)
-        phase_three(MOTOR_FOUR_PINS)
+        # phase_three(MOTOR_FOUR_PINS)
+        GPIO.output(MOTOR_FOUR_PINS[0], GPIO.HIGH)
         time.sleep(time_sleep)
     
         phase_four(MOTOR_ONE_PINS)
         phase_four(MOTOR_TWO_PINS)
         phase_four(MOTOR_THREE_PINS)
-        phase_four(MOTOR_FOUR_PINS)
+        # phase_four(MOTOR_FOUR_PINS)
+        GPIO.output(MOTOR_FOUR_PINS[0], GPIO.LOW)
         time.sleep(time_sleep)
 
 
@@ -182,25 +197,25 @@ def drive_south(motor_one_pins=MOTOR_ONE_PINS, motor_two_pins=MOTOR_TWO_PINS, mo
         phase_one(MOTOR_ONE_PINS)
         phase_one_reversed(MOTOR_TWO_PINS)
         phase_one(MOTOR_THREE_PINS)
-        phase_one_reversed(MOTOR_FOUR_PINS)
+        # phase_one_reversed(MOTOR_FOUR_PINS)
         time.sleep(time_sleep)
         
         phase_two(MOTOR_ONE_PINS)
         phase_two(MOTOR_TWO_PINS)
         phase_two(MOTOR_THREE_PINS)
-        phase_two(MOTOR_FOUR_PINS)
+        # phase_two(MOTOR_FOUR_PINS)
         time.sleep(time_sleep)
         
         phase_three(MOTOR_ONE_PINS)
         phase_three_reversed(MOTOR_TWO_PINS)
         phase_three(MOTOR_THREE_PINS)
-        phase_three_reversed(MOTOR_FOUR_PINS)
+        # phase_three_reversed(MOTOR_FOUR_PINS)
         time.sleep(time_sleep)
         
         phase_four(MOTOR_ONE_PINS)
         phase_four(MOTOR_TWO_PINS)
         phase_four(MOTOR_THREE_PINS)
-        phase_four(MOTOR_FOUR_PINS)
+        # phase_four(MOTOR_FOUR_PINS)
         time.sleep(time_sleep)
 
 
@@ -211,25 +226,25 @@ def drive_east(motor_one_pins=MOTOR_ONE_PINS, motor_two_pins=MOTOR_TWO_PINS, mot
         phase_one(MOTOR_ONE_PINS)
         phase_one_reversed(MOTOR_TWO_PINS)
         phase_one_reversed(MOTOR_THREE_PINS)
-        phase_one(MOTOR_FOUR_PINS)
+        # phase_one(MOTOR_FOUR_PINS)
         time.sleep(time_sleep)
         
         phase_two(MOTOR_ONE_PINS)
         phase_two(MOTOR_TWO_PINS)
         phase_two(MOTOR_THREE_PINS)
-        phase_two(MOTOR_FOUR_PINS)
+        # phase_two(MOTOR_FOUR_PINS)
         time.sleep(time_sleep)
         
         phase_three(MOTOR_ONE_PINS)
         phase_three_reversed(MOTOR_TWO_PINS)
         phase_three_reversed(MOTOR_THREE_PINS)
-        phase_three(MOTOR_FOUR_PINS)
+        # phase_three(MOTOR_FOUR_PINS)
         time.sleep(time_sleep)
         
         phase_four(MOTOR_ONE_PINS)
         phase_four(MOTOR_TWO_PINS)
         phase_four(MOTOR_THREE_PINS)
-        phase_four(MOTOR_FOUR_PINS)
+        # phase_four(MOTOR_FOUR_PINS)
         time.sleep(time_sleep)
 
 
@@ -241,25 +256,25 @@ def drive_west(motor_one_pins=MOTOR_ONE_PINS, motor_two_pins=MOTOR_TWO_PINS, mot
         phase_one(MOTOR_ONE_PINS)
         phase_one(MOTOR_TWO_PINS)
         phase_one_reversed(MOTOR_THREE_PINS)
-        phase_one_reversed(MOTOR_FOUR_PINS)
+        # phase_one_reversed(MOTOR_FOUR_PINS)
         time.sleep(time_sleep)
         
         phase_two(MOTOR_ONE_PINS)
         phase_two(MOTOR_TWO_PINS)
         phase_two(MOTOR_THREE_PINS)
-        phase_two(MOTOR_FOUR_PINS)
+        # phase_two(MOTOR_FOUR_PINS)
         time.sleep(time_sleep)
         
         phase_three(MOTOR_ONE_PINS)
         phase_three(MOTOR_TWO_PINS)
         phase_three_reversed(MOTOR_THREE_PINS)
-        phase_three_reversed(MOTOR_FOUR_PINS)
+        # phase_three_reversed(MOTOR_FOUR_PINS)
         time.sleep(time_sleep)
         
         phase_four(MOTOR_ONE_PINS)
         phase_four(MOTOR_TWO_PINS)
         phase_four(MOTOR_THREE_PINS)
-        phase_four(MOTOR_FOUR_PINS)
+        # phase_four(MOTOR_FOUR_PINS)
         time.sleep(time_sleep)
 
 
@@ -268,19 +283,23 @@ def drive_diag_NE(motor_one_pins=MOTOR_ONE_PINS, motor_two_pins=MOTOR_TWO_PINS, 
     # front right/back left don't move, front left/back right move forward
     for _ in range(NUM_STEPS_PER_CMD):
         phase_one_reversed(MOTOR_ONE_PINS)
-        phase_one(MOTOR_FOUR_PINS)
+        # phase_one(MOTOR_FOUR_PINS)
+        GPIO.output(MOTOR_FOUR_PINS[0], GPIO.HIGH)
         time.sleep(time_sleep)
         
         phase_two(MOTOR_ONE_PINS)
-        phase_two(MOTOR_FOUR_PINS)
+        # phase_two(MOTOR_FOUR_PINS)
+        GPIO.output(MOTOR_FOUR_PINS[0], GPIO.LOW)
         time.sleep(time_sleep)
         
         phase_three_reversed(MOTOR_ONE_PINS)
-        phase_three(MOTOR_FOUR_PINS)
+        # phase_three(MOTOR_FOUR_PINS)
+        GPIO.output(MOTOR_FOUR_PINS[0], GPIO.HIGH)
         time.sleep(time_sleep)
         
         phase_four(MOTOR_ONE_PINS)
-        phase_four(MOTOR_FOUR_PINS)
+        # phase_four(MOTOR_FOUR_PINS)
+        GPIO.output(MOTOR_FOUR_PINS[0], GPIO.LOW)
         time.sleep(time_sleep)
 
 
@@ -407,9 +426,13 @@ def rotate_counter_clock(motor_one_pins=MOTOR_ONE_PINS, motor_two_pins=MOTOR_TWO
 
 # print("running")
 setup_all_pins()
-# run_motors(MOTOR_ONE_PINS, MOTOR_TWO_PINS, MOTOR_THREE_PINS, MOTOR_FOUR_PINS, 0.002, 500)
-drive_north()
+
+run_motors(MOTOR_ONE_PINS, MOTOR_TWO_PINS, MOTOR_THREE_PINS, MOTOR_FOUR_PINS, 0.002, 500)
+
+# drive_north()
+
 print("done")
+
 # time.sleep(1)
 # print("reversing")
 # reverse_motors(MOTOR_ONE_PINS, MOTOR_TWO_PINS, MOTOR_THREE_PINS, MOTOR_FOUR_PINS, 0.002, 500)
@@ -425,9 +448,3 @@ print("done")
 
 
 GPIO.cleanup()
-
-
-
-
-
-# TODO: make reverse function work. make half stepping
