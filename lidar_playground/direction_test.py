1c197641bb3831d557d4c19c23b773eee95570ab
ayushchakra <achakraborty@olin.edu>
2022-12-06 21:53:07 -0500

    added arduino support to lidar

--- lidar_playground/direction_test.py
+++ lidar_playground/direction_test.py
@@ -6,16 +6,21 @@ port = '/dev/ttyUSB0'
 lidar = RPLidar(port)
 
 curr_scan = None
-
 angle_by_dir = {
-    'N': [337.5, 22.5],
-    'NE': [22.5, 67.5],
-    'E': [67.5, 112.5],
-    'SE': [112.5, 157.5],
-    'S': [157.5, 202.5],
-    'SW': [202.5, 247.5],
-    'W': [247.5, 292.5],
-    'NW': [292.5, 337.5]
+    'S': [337.5, 22.5],
+    'SW': [45, 135],
+    'N': [135, 225],
+    'E': [225, 315]
+}
+angle_by_dir = {
+    'S': [337.5, 22.5],
+    'SW': [22.5, 67.5],
+    'W': [67.5, 112.5],
+    'NW': [112.5, 157.5],
+    'N': [157.5, 202.5],
+    'NE': [202.5, 247.5],
+    'E': [247.5, 292.5],
+    'SE': [292.5, 337.5]
 }
 close_points_by_dir = {
     'N': 0,
@@ -42,10 +47,10 @@ while curr_scan is None:
         continue
 
 for direction, angle_range in angle_by_dir.items():
-    if direction == 'N':
-        close_points_by_dir[direction] = len([point for point in curr_scan if (point[1] > angle_range[0] or point[1] < angle_range[1]) and point[0] < 1000])
+    if direction == 'S':
+        close_points_by_dir[direction] = len([point for point in curr_scan if (point[1] > angle_range[0] or point[1] < angle_range[1]) and point[2] < 1000])
     else:
-        close_points_by_dir[direction] = len([point for point in curr_scan if point[1] > angle_range[0] and point[1] < angle_range[1] and point[0] < 1000])
+        close_points_by_dir[direction] = len([point for point in curr_scan if point[1] > angle_range[0] and point[1] < angle_range[1] and point[2] < 1000])
 
 print(close_points_by_dir)
 print(min(close_points_by_dir, key=close_points_by_dir.get))
--- lidar_playground/rplidar_test.py
+++ lidar_playground/rplidar_test.py
@@ -12,15 +12,15 @@ lidar = RPLidar('/dev/ttyUSB0')
 # health = lidar.get_health()
 # print(health)
 
-# x = []
-# y = []
+x = []
+y = []
 
-# plt.ion()
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# line1, = ax.plot(x,y,'b.')
-# ax.set_xbound(lower=-1000, upper=1000)
-# ax.set_ybound(lower=-1000, upper=1000)
+plt.ion()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+line1, = ax.plot(x,y,'b.')
+ax.set_xbound(lower=-100, upper=100)
+ax.set_ybound(lower=-100, upper=100)
 
 close_points_by_dir = {
     'N': 0,
@@ -28,14 +28,23 @@ close_points_by_dir = {
     'S': 0,
     'W': 0,
 }
+# angle_by_dir = {
+#     'E': [315, 45],
+#     'N': [45, 135],
+#     'W': [135, 225],
+#     'S': [225, 315],
+# }
+
 angle_by_dir = {
-    'E': [315, 45],
-    'N': [45, 135],
-    'W': [135, 225],
-    'S': [225, 315],
+    'S': [315, 45],
+    'W': [45, 135],
+    'N': [135, 225],
+    'E': [225, 315]
 }
 
 for i, scan in enumerate(lidar.iter_scans()):
+    print(scan)
+    pdb.set_trace()
     angles = [point[1] for point in scan]
     dists = [point[2]/10 for point in scan]
     for direction, angle_range in angle_by_dir.items():
@@ -46,28 +55,28 @@ for i, scan in enumerate(lidar.iter_scans()):
 
     next_dir = min(close_points_by_dir, key=close_points_by_dir.get)
 
-    if next_dir == 'N':
-        plt.quiver(0, 0, 0, 1000)
-    if next_dir == 'E':
-        plt.quiver(0, 0, 1000, 0)
-    if next_dir == 'S':
-        plt.quiver(0, 0, 0, -1000)
-    if next_dir == 'W':
-        plt.quiver(0, 0, -1000, 0)
+    # if next_dir == 'N':
+    #     plt.quiver(0, 0, 0, 1000)
+    # if next_dir == 'E':
+    #     plt.quiver(0, 0, 1000, 0)
+    # if next_dir == 'S':
+    #     plt.quiver(0, 0, 0, -1000)
+    # if next_dir == 'W':
+    #     plt.quiver(0, 0, -1000, 0)
 
     x = dists * np.cos(np.deg2rad(angles))
     y = dists * np.sin(np.deg2rad(angles))
-    plt.plot(x, y, 'o', label='LiDAR Scan')
-    plt.plot(0, 0, 'ko', markersize=1, label='Robot Drive Velocity')
-    plt.title('LiDAR Processing Simulation')
-    plt.xlabel('x (cm)')
-    plt.ylabel('y (cm)')
-    plt.legend()
-    plt.show()
-    # line1.set_xdata(x)
-    # line1.set_ydata(y)
-    # fig.canvas.draw()
-    # fig.canvas.flush_events()
+    # plt.plot(x, y, 'o', label='LiDAR Scan')
+    # plt.plot(0, 0, 'ko', markersize=1, label='Robot Drive Velocity')
+    # plt.title('LiDAR Processing Simulation')
+    # plt.xlabel('x (cm)')
+    # plt.ylabel('y (cm)')
+    # plt.legend()
+    # plt.show()
+    line1.set_xdata(x)
+    line1.set_ydata(y)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
 
 
 lidar.stop()
